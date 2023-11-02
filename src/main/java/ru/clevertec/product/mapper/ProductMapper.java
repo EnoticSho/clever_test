670669777ad8f6d809cc84c7d1b5e027c8b717b6
sergey turpakov <paradise666hell@mail.ru>
2023-11-02 10:41:41 +0300

    fix(impl): fixed some marks

--- src/main/java/ru/clevertec/product/mapper/ProductMapper.java
+++ src/main/java/ru/clevertec/product/mapper/ProductMapper.java
@@ -16,6 +16,8 @@ public interface ProductMapper {
      * @param productDto - DTO для маппинга
      * @return новый продукт
      */
+    @Mapping(target = "uuid", ignore = true)
+    @Mapping(target = "created", ignore = true)
     Product toProduct(ProductDto productDto);
 
     /**
